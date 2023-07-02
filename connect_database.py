ba67f065c662880d770822bdc770025d74846616
RakshithJKashyap <rakshithj07@gmail.com>
2023-07-02 13:49:53 +0530

    db connected

---
+++ connect_database.py
@@ -0,0 +1,24 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+
+cassandra_username = os.getenv("CASSANDRA_USERNAME")
+cassandra_password = os.getenv("CASSANDRA_PASSWORD")
+
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-task2.zip'
+}
+auth_provider = PlainTextAuthProvider(cassandra_username, cassandra_password)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+row = session.execute("select release_version from system.local").one()
+if row:
+  print(row[0])
+else:
+  print("An error occurred.")
